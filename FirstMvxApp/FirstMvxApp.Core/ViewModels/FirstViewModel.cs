fef8f84b156ebca92b4ce20a6dc0ed0b9bdb5877
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2013-12-05 10:54:19 +0800

    simple view model logic

--- FirstMvxApp/FirstMvxApp.Core/ViewModels/FirstViewModel.cs
+++ FirstMvxApp/FirstMvxApp.Core/ViewModels/FirstViewModel.cs
@@ -1,10 +1,50 @@
 using System;
 using Cirrious.MvvmCross.ViewModels;
+using System.Windows.Input;
 
 namespace FirstMvxApp.ViewModels {
 
 	public class FirstViewModel : MvxViewModel {
 
+		private string firstName;
+		private string lastName;
+		private string fullName;
+
+		public string FirstName {
+			get {
+				return firstName;
+			}
+			set {
+				firstName = value;
+				RaisePropertyChanged("FirstName");
+			}
+		}
+
+		public string LastName {
+			get {
+				return lastName;
+			}
+			set {
+				lastName = value;
+				RaisePropertyChanged("LastName");
+			}
+		}
+
+		public string FullName {
+			get {
+				return fullName;
+			}
+			set {
+				fullName = value;
+				RaisePropertyChanged("FullName");
+			}
+		}
+
+		private ICommand GetFullName {
+			get {
+				return new MvxCommand(() => FullName = string.Format("{0} {1}", FirstName, LastName));
+			}
+		}
 	}
 }
 
